dd39dfda13e2af9e1177d8a48c54c496461dcdf0
programanda03 <paganiamanda791@gmail.com>
2025-04-10 02:03:44 -0300

    add SetTrustedProxies to main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,7 +8,7 @@ func main() {
 	// Define os proxies confiáveis
 	// Use []string{"0.0.0.0/0"} para confiar em todos (não recomendado em produção)
 	// Ou especifique o IP/cidr exato do proxy (ex: "192.168.1.1" ou "10.0.0.0/8")
-	err := eng.SetTrustedProxies([]string{"127.0.0.1", "189.33.123.82"})
+	err := eng.SetTrustedProxies([]string{"127.0.0.1"})
 	if err != nil {
 		panic(err)
 	}
